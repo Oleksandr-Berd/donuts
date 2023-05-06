693dcf818f7953a038b9503e27e01ceee65f08b3
Oleksandr-Berd <alex.berd86@gmail.com>
2023-05-06 18:54:05 +0300

    made programCards

--- src/Components/Program/Program.jsx
+++ src/Components/Program/Program.jsx
@@ -1,32 +1,30 @@
 import ReadBtn from "../ReadBtn/ReadBtn";
-import styles from "./Program.module.css"
+import styles from "./Program.module.css";
+import ProgramCards from "./ProgramCards/ProgramCards";
 
 const Program = () => {
-    return (
-      <section className={styles.section}>
-        <h2 className={styles.title}>Program</h2>
-        <p className={styles.text}>
-          Indulge in this immersive sweet treat experience with The Chef Donut
-          Queen in her New York donut shop!
-        </p>
-        <ul className={styles.list}>
-          <li className={styles.item}>A stand up history of donuts</li>
-          <li className={styles.item}>Make 3 donut flavors of your choice</li>
-          <li className={styles.item}>Enjoy all-you-can-eat donuts</li>
-          <li className={styles.item}>Cocktail pairings and coffee included</li>
-          <li className={styles.item}>
-            Photoshoot by professional photographer
-          </li>
-          <li className={styles.item}>Secret receipt of cooking from Eva</li>
-          <li className={styles.item}>
-            All products are included in the price
-          </li>
-        </ul>
-        <div className={styles.btnCon}>
-          <ReadBtn />
-        </div>
-      </section>
-    );
-}
- 
+  return (
+    <section className={styles.section}>
+      <h2 className={styles.title}>Program</h2>
+      <p className={styles.text}>
+        Indulge in this immersive sweet treat experience with The Chef Donut
+        Queen in her New York donut shop!
+      </p>
+      <ul className={styles.list}>
+        <li className={styles.item}>A stand up history of donuts</li>
+        <li className={styles.item}>Make 3 donut flavors of your choice</li>
+        <li className={styles.item}>Enjoy all-you-can-eat donuts</li>
+        <li className={styles.item}>Cocktail pairings and coffee included</li>
+        <li className={styles.item}>Photoshoot by professional photographer</li>
+        <li className={styles.item}>Secret receipt of cooking from Eva</li>
+        <li className={styles.item}>All products are included in the price</li>
+      </ul>
+      <div className={styles.btnCon}>
+        <ReadBtn />
+      </div>
+      <ProgramCards />
+    </section>
+  );
+};
+
 export default Program;
--- src/Components/Program/ProgramCards/ProgramCards.jsx
+++ src/Components/Program/ProgramCards/ProgramCards.jsx
@@ -1,12 +1,54 @@
 import styles from "./ProgramCards.module.css"
 
+import img from "../../../Utils/images/Frame 23.png";
+import img2 from "../../../Utils/images/Frame 24.png";
+import img3 from "../../../Utils/images/Frame 25.png";
+import img4 from "../../../Utils/images/Frame 26.png";
+import icon from "../../../Utils/images/Frame 19.png";
+import icon2 from "../../../Utils/images/Frame 20.png";
+import icon3 from "../../../Utils/images/Frame 21.png";
+import icon4 from "../../../Utils/images/Frame 22.png";
+
+
+
 const ProgramCards = () => {
     return (
       <ul className={styles.cadsList}>
-        <li className={styles.cardsItem}></li>
-        <li className={styles.cardsItem}></li>
-        <li className={styles.cardsItem}></li>
-        <li className={styles.cardsItem}></li>
+        <li className={styles.cardsItem}>
+          <img src={img} alt="donut" className={styles.img} />
+          <div className={styles.cardCon}>
+            <h3 className={styles.cardTitle}>Type of the event</h3>
+            <p className={styles.cardText}>Cooking classes</p>
+            <img src={icon} alt="icon" />
+          </div>
+        </li>
+        <li className={styles.cardsItem}>
+          <img src={img2} alt="donut" className={styles.img} />
+
+          <div className={styles.cardCon}>
+            <h3 className={styles.cardTitle}>Number of guests</h3>
+            <p className={styles.cardText}>1 to 20</p>
+            <img src={icon2} alt="icon" />
+          </div>
+        </li>
+        <li className={styles.cardsItem}>
+          <img src={img3} alt="donut" className={styles.img} />
+
+          <div className={styles.cardCon}>
+            <h3 className={styles.cardTitle}>Time of the event</h3>
+            <p className={styles.cardText}>18:30 (2.5 hours)</p>
+            <img src={icon3} alt="icon" />
+          </div>
+        </li>
+        <li className={styles.cardsItem}>
+          <img src={img4} alt="donut" className={styles.img} />
+
+          <div className={styles.cardCon}>
+            <h3 className={styles.cardTitle}>The host speaks</h3>
+            <p className={styles.cardText}>English, French</p>
+            <img src={icon4} alt="icon" />
+          </div>
+        </li>
       </ul>
     );
 }
