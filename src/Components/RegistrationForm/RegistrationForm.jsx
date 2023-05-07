7708976e9687419caa9519d5ebdc752d83927d06
Oleksandr-Berd <alex.berd86@gmail.com>
2023-05-07 18:36:18 +0300

    made validation

--- src/Components/RegistrationForm/RegistrationForm.jsx
+++ src/Components/RegistrationForm/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
 import styles from "./RegistrationForm.module.css";
@@ -7,13 +7,9 @@ import { postUser } from "../../Utils/helpers";
 import { registrationUrl } from "../../Utils/url";
 
   const validationSchema = Yup.object().shape({
-    name: Yup.string()
-      .required("Name is required")
-      .min(2, "Name must be at least 2 characters"),
+    name: Yup.string().min(2, "Too Short!").required("Name is required"),
     email: Yup.string().email("Invalid email").required("Email is required"),
-    phone: Yup.string()
-      .required("Phone is required")
-      .min(7, "Phone must be at least 7 characters"),
+    phone: Yup.string().min(7, "Too Short!").required("Phone is required"),
   });
 
 
@@ -58,71 +54,81 @@ const RegistrationForm = () => {
         }}
         validationSchema={validationSchema}
       >
-        <Form className={styles.form} onSubmit={handleSubmit}>
-          <Field
-            id="name"
-            name="name"
-            value={data.name}
-            placeholder=""
-            className={styles.input}
-            onChange={handleInput}
-          />
-          {/* <ErrorMessage name="name" /> */}
-          {data.name === "" ? (
-            <label htmlFor="name" className={styles.label}>
-              Your name
-            </label>
-          ) : (
-            <label
-              htmlFor="name"
-              className={styles.label}
-              style={{ display: "none" }}
-            >
-              Your name
-            </label>
-          )}
+        {({ errors, touched }) => (
+          <Form className={styles.form} onSubmit={handleSubmit}>
+            <Field
+              id="name"
+              name="name"
+              value={data.name}
+              placeholder=""
+              className={styles.input}
+              onChange={handleInput}
+            />
 
-          <Field
-            id="phone"
-            name="phone"
-            placeholder=""
-            value={data.phone}
-            className={styles.input}
-            onChange={handleInput}
-          />
-          {/* <ErrorMessage name="phone" /> */}
-          {data.phone === "" ? (
-            <label htmlFor="phone" className={styles.label}>
-              Phone number
-            </label>
-          ) : (
-            <label htmlFor="phone" style={{ display: "none" }}>
-              Phone number
-            </label>
-          )}
-          <Field
-            id="email"
-            name="email"
-            placeholder=""
-            value={data.email}
-            type="email"
-            className={styles.input}
-            onChange={handleInput}
-          />
-          {/* <ErrorMessage name="email" /> */}
-          {data.email === "" ? (
-            <label htmlFor="email" className={styles.label}>
-              Email
-            </label>
-          ) : (
-            <label htmlFor="email" style={{ display: "none" }}>
-              Email
-            </label>
-          )}
-          <button type="submit" className={styles.btn}>
-            Register
-          </button>
-        </Form>
+            {data.name === "" && !errors.name ? (
+              <label htmlFor="name" className={styles.label}>
+                Your name
+              </label>
+            ) : (
+              <label
+                htmlFor="name"
+                className={styles.label}
+                style={{ display: "none" }}
+              >
+                Your name
+              </label>
+            )}
+            {errors.name && touched.name ? (
+              <div className={styles.errorCon}>{errors.name}</div>
+            ) : null}
+            <Field
+              id="phone"
+              name="phone"
+              placeholder=""
+              value={data.phone}
+              className={styles.input}
+              onChange={handleInput}
+            />
+            {data.phone === "" && !errors.phone ? (
+              <label htmlFor="phone" className={styles.label}>
+                Phone number
+              </label>
+            ) : (
+              <label htmlFor="phone" style={{ display: "none" }}>
+                Phone number
+              </label>
+            )}
+            {errors.phone && touched.phone ? (
+              <div className={styles.errorCon}>{errors.phone}</div>
+            ) : null}
+
+            <Field
+              id="email"
+              name="email"
+              placeholder=""
+              value={data.email}
+              type="email"
+              className={styles.input}
+              onChange={handleInput}
+            />
+
+            {data.email === "" && !errors.email ? (
+              <label htmlFor="email" className={styles.label}>
+                Email
+              </label>
+            ) : (
+              <label htmlFor="email" style={{ display: "none" }}>
+                Email
+              </label>
+            )}
+            {errors.email && touched.email ? (
+              <div className={styles.errorCon}>{errors.email}</div>
+            ) : null}
+            <button type="submit" className={styles.btn}>
+              Register
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
