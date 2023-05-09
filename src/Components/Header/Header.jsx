3428763580836dc8d950586eee8a89c03dc3c74e
Oleksandr-Berd <alex.berd86@gmail.com>
2023-05-09 12:26:39 +0300

    start tablet version header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,21 @@
+import { useMediaQuery } from "react-responsive";
+
 import Logo from "../Logo/Logo";
 import Menu from "../Menu/Menu";
 import RegisterBtn from "../RegisterBtn/RegisterBtn";
 import SocIcons from "../SocIcons/SocIcons";
 import styles from "./Header.module.css";
+import {ReactComponent as DonutsSvg} from "../../Utils/images/pic donuts_tablet.svg"
 
-const Header = ({toggleModal}) => {
+const Header = ({ toggleModal }) => {
+    const isTablet = useMediaQuery({ minWidth: 768 });
+
+
   return (
     <header className={styles.header}>
       <div className={styles.iconsCon}>
         <Logo />
+        {isTablet && <DonutsSvg className={styles.donuts } />}
         <Menu toggleModal={toggleModal} />
       </div>
       <div className={styles.textCon}>
